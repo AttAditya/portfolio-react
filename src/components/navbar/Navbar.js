cec2c94ff72d5b18f93bf335b5c90cee0ef8e2b2
AttAditya <attachment.aditya@gmail.com>
2024-05-27 13:29:08 +0530

    Updated navbar, looks really nice now, also functional

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,28 @@
+import { useRef } from 'react';
+
+import pixelAditya from '../../assets/images/PixelAditya.png';
 import './Navbar.css';
+import Loki from '../texts/Loki';
+import Gradient from '../texts/Gradient';
 
 function Navbar({ nav_sections }) {
+    let navbarRef = useRef();
+
+    function toggleNavbar() {
+        navbarRef.current.classList.toggle("active");
+    }
+
     return (
-        <nav className="navbar">
-            <ul>
+        <nav className="navbar" ref={navbarRef}>
+            <button className="navbar-logo" onClick={toggleNavbar}>
+                <img src={pixelAditya} alt="Pixel Aditya" />
+            </button>
+
+            <Gradient text={
+                (<Loki text="ADITYA" />)
+            } />
+
+            <ul className="navbar-links">
                 {
                     nav_sections.map((section, index) => (
                         <li key={index} >
@@ -11,6 +30,13 @@ function Navbar({ nav_sections }) {
                         </li>
                     ))
                 }
+            </ul>
+
+            <ul className="navbar-social">
+                <li>
+                    <a href="https://instagram.com/attachment-aditya" target="_blank" rel="noreferrer">
+                    </a>
+                </li>
             </ul>
         </nav>
     );
