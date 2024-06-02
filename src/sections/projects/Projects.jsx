c1381136164b1ea6fb0b3b7987508883ae10589d
AttAditya <attachment.aditya@gmail.com>
2024-06-03 01:48:42 +0530

    Updated navbar for responsiveness and added more projects to projects data, plus added project account links on projects section

--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -2,8 +2,28 @@ import "./Projects.css";
 
 import { Gradient, ProjectCard } from "../../components";
 import projectsData from "./ProjectsData";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faItchIo } from "@fortawesome/free-brands-svg-icons";
 
 function Projects() {
+    let accountsData = [
+        {
+            url: "https://github.com/AttAditya",
+            icon: faGithub,
+            name: "AttAditya"
+        },
+        {
+            url: "https://github.com/Attachment-Studios",
+            icon: faGithub,
+            name: "Attachment Studios(GitHub)"
+        },
+        {
+            url: "https://attachment-studios.itch.io/",
+            icon: faItchIo,
+            name: "Attachment Studios(itch.io)"
+        }
+    ];
+
     return (
         <section className="section" id="projects">
             <h1 className="section-title">
@@ -12,6 +32,29 @@ function Projects() {
 
             <div className="section-content">
                 <div className="main-content">
+                    <div className="accounts">
+                        <h2 className="accounts-title">
+                            Find my projects on
+                        </h2>
+
+                        <ul className="accounts-list">
+                            {
+                                accountsData.map((account, index) => (
+                                    <li key={index} className="account">
+                                        <a href={account.url} target="_blank" rel="noopener noreferrer" className="account-link">
+                                            <button className="account-button">
+                                                <FontAwesomeIcon icon={account.icon} className="account-icon" />
+                                                <span className="account-name">
+                                                    <Gradient text={account.name} />
+                                                </span>
+                                            </button>
+                                        </a>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+
                     <ul className="projects-list">
                         {
                             projectsData.map((project, index) => (
